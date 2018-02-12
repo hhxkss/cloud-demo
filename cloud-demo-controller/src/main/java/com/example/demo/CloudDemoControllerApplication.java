4d34d450abcdcd0f8458f8b34211459a90e3409d
kss48442 <kss48442@ly.com>
2018-02-12 15:59:18 +0800

    eureka

--- cloud-demo-controller/src/main/java/com/example/demo/CloudDemoControllerApplication.java
+++ cloud-demo-controller/src/main/java/com/example/demo/CloudDemoControllerApplication.java
@@ -2,10 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
+@EnableDiscoveryClient
 public class CloudDemoControllerApplication {
 
 	public static void main(String[] args) {
--- cloud-demo-service/src/main/java/com/example/demo/CloudDemoServiceApplication.java
+++ cloud-demo-service/src/main/java/com/example/demo/CloudDemoServiceApplication.java
@@ -3,10 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.example.demo.dao")
 public class CloudDemoServiceApplication {
 
